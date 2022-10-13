7156debe26ca92e84d02ca898d423860457b05de
Eduardo Casamian <casamianeduardogmail.com>
2022-10-13 18:57:32 +0100

    ejform1(commit on '01inicio')

--- data/ejercicios/solucionest2/autorizaLibros.php
+++ data/ejercicios/solucionest2/autorizaLibros.php
@@ -5,8 +5,8 @@
     <title>Document</title>
 </head>
 <body>
-    <h1>Pagina de chequeo de credenciales : </h1>
-    <h2>Si has llegado aqui eres un heroe</h2>
+    <h1>Pagina de chequeo de credenciales de libro : </h1>
+    <h2>Datos introducidos</h2>
     <?php
 
     if(isset($_GET['envio'])) {
@@ -21,11 +21,31 @@
     if(isset($_GET['envio'])) {
         if (!empty($_GET['autor'])  ){
         $autor = $_GET ['autor'];
-        echo "<br>Titulo introducido : " . $autor;
+        echo "<br>Autor introducido : " . $autor;
     }else{
      echo "<br><h3>No has introducido ningun autor</h3>";
          }
     }
+
+    if(isset($_GET['envio'])) {
+        if (!empty($_GET['editorial'])  ){
+        $editorial = $_GET ['editorial'];
+        echo "<br>Editorial introducido : " . $editorial;
+    }else{
+     echo "<br><h3>No has introducido ninguna editorial</h3>";
+         }
+    }
+
+    if(isset($_GET['envio'])) {
+        if (!empty($_GET['paginas'])  ){
+        $paginas = $_GET ['paginas'];
+        echo "<br>paginas introducido : " . $paginas;
+    }else{
+     echo "<br><h3>No has introducido ninguna paginas</h3>";
+         }
+    }
+
+    
     ?>
 </body>
 </html>
