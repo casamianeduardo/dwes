cadbb0ab0a37ab517ec957e399bd1572a8c202e7
Eduardo Casamian <casamianeduardogmail.com>
2022-10-19 17:51:57 +0100

    commitclase(commit on '01inicio')

--- data/ejercicios/cookies.php
+++ data/ejercicios/cookies.php
@@ -3,12 +3,21 @@
 //time() = strtotime(now()); 
 //epoch time(epoch converter.com), tiempo en segundos desde 1970 = strtotime()
 
-$miarray = ["hola",25,[1,3,5]];
-    $infoguardar = serialize($miarray);//Devuelve una cadena 
+$miarray = ['a'=>"hola",'b'=>25,'c'=>[1,3,5]];
+$infoguardar = serialize($miarray);//Devuelve una cadena 
+
+$infojson = json_encode($miarray);
+echo "<br>Codificacion del array en json: " . $infojson;
+
+$micad = '{"a":"hola","b":10}' ;
+var_dump(json_decode($micad));
+
+
+
 
 //definir una cookie
-setcookie("primeracookie","aceptado",time()+60); //duracion de la cookie 1 minuto
-setcookie("segundacookie",$infoguardar,time()+60);
+//setcookie("primeracookie",$infoguardar,time()+60); //duracion de la cookie 1 minuto
+//setcookie("segundacookie",$infojson,time()+60);
 
  //las cookies se almacenan en un array asociativo, que contiene la informacion de las cookies
 
@@ -24,6 +33,8 @@ echo "<h1>Mi primera cookie</h1>";
 
 echo "<br>Valor de la cookie 1 : " . $_COOKIE["primeracookie"];
 echo "<pre>";
-var_dump(unserialize($_COOKIE["segundacookie"])); 
+echo "<br>Valor de cookie 2: ";
+var_dump($infojson);
+//var_dump(unserialize($_COOKIE["segundacookie"])); 
 
 echo "<br>Numero de cookies creadas " .count($_COOKIE);
