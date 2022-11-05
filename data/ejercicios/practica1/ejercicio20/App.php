2cae0c794d653315bd5ce967c96d3bfd3ed7633a
Eduardo Casamian <casamianeduardogmail.com>
2022-11-05 12:40:22 +0000

    commitultimoejercicio(commit on '01inicio')

--- data/ejercicios/practica1/ejercicio20/App.php
+++ data/ejercicios/practica1/ejercicio20/App.php
@@ -14,24 +14,23 @@ class App{
     }
 
 
-    public function home(){
+    public function home(){ //Comprueba si hay una cookie llamada "color".
 
-        if (isset($_COOKIE['color'])) {
-            header('Location: index.php?method=colores');
-        } else {
-            include('views/colores.php');
+        if (isset($_COOKIE['color'])) {//si la cokie color existe:
+            header('Location: index.php?method=colores');//redirigir a metodo colores
+        } else {//si cookie color no existe:
+            include('views/colores.php');//include vista colores
         }
     }
 
-    public function cambio()
-    {
+    public function cambio(){//Recibe el color de la página anterior, crea la cookie y reenvia al metodo colores
 
-        $color = $_GET['color'];
-        setcookie('color', $color, time() + 60 * 60 * 4);
-        header('Location: index.php?method=colores');
+        $color = $_GET['color'];//guarda en la variable color lo que recibe de $_GET['color']
+        setcookie('color', $color, time() + 60 * 60 * 4);//establecer cookie
+        header('Location: index.php?method=colores');//redirige al metodo colores
     }
 
-    public function colores(){
+    public function colores(){//comprueb la cookie color y pone el color de fondo que recibe en ella , include vista home
 
         if (($_COOKIE['color']) == "red") {
             echo '<body style="background-color:red">';
@@ -48,6 +47,6 @@ class App{
         if (($_COOKIE['color']) == "green") {
             echo '<body style="background-color:green">';
         }
-        include('views/home.php');
+        include('views/home.php');//include vista home
     }
 }
